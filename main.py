ea7ed4341ba785c7d58d303eddbac257c12f8abb
洛音 <czb381147@qq.com>
2022-08-27 14:38:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -61,8 +61,6 @@ def get_weather(region):
     # 获取逐日天气预报
     url = "https://devapi.qweather.com/v7/weather/3d?location={}&key={}".format(location_id, key)
     response = get(url, headers=headers).json()
-    # 降水概率
-    pop = response["daily"][0]["pop"]
     # 最高气温
     max_temp = response["daily"][0]["tempMax"] + u"\N{DEGREE SIGN}" + "C"
     # 最低气温
@@ -88,7 +86,7 @@ def get_weather(region):
     proposal = ""
     if response["code"] == "200":
         proposal += response["daily"][0]["text"]
-    return weather, temp, max_temp, min_temp, wind_dir, sunrise, sunset, category, pm2p5, proposal, pop
+    return weather, temp, max_temp, min_temp, wind_dir, sunrise, sunset, category, pm2p5, proposal
 
 
 def get_tianhang():
@@ -251,11 +249,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
             "chp": {
                 "value": chp,
                 "color": get_color()
-            },
-            "pop": {
-                "value": pop,
-                "color": get_color()
-            },
+            },        
 
         }
     }
@@ -305,7 +299,7 @@ if __name__ == "__main__":
     users = config["user"]
     # 传入地区获取天气信息
     region = config["region"]
-    weather, temp, max_temp, min_temp, wind_dir, sunrise, sunset, category, pm2p5, proposal, pop = get_weather(region)
+    weather, temp, max_temp, min_temp, wind_dir, sunrise, sunset, category, pm2p5, proposal = get_weather(region)
     note_ch = config["note_ch"]
     note_en = config["note_en"]
     if note_ch == "" and note_en == "":
@@ -315,5 +309,5 @@ if __name__ == "__main__":
     # 公众号推送消息
     for user in users:
         send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en, max_temp, min_temp, sunrise,
-                     sunset, category, pm2p5, proposal, chp, pop)
+                     sunset, category, pm2p5, proposal, chp)
     os.system("pause")
